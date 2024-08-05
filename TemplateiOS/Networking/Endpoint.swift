67124b787664736eff865c4e7f860d80bfcf2c7f
sandesh-naik <sandesh.naik@allen.in>
2024-08-05 16:57:07 +0530

    Initial Commit

---
+++ TemplateiOS/Networking/Endpoint.swift
@@ -0,0 +1,42 @@
+//
+//  Endpoint.swift
+//  Template
+//
+//  Created by Rameez Khan on 17/10/21.
+//
+
+import Foundation
+
+protocol Endpoint {
+    // HTTP or HTTPS
+    var scheme: String { get }
+    
+    // Example itunes.apple.com
+    var baseURL: String { get }
+    
+    // /search/
+    var path: String { get }
+    
+    // search?term=taylor+swift
+    var params: [URLQueryItem] { get }
+    
+    var method: String { get }
+    
+    func generateURLRequest() -> URLRequest?
+}
+
+
+extension Endpoint {
+    func generateURLRequest() -> URLRequest? {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = baseURL
+        components.path = path
+        components.queryItems = queryItems
+        
+        guard let url = components.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        return request
+    }
+}
---
+++ TemplateiOS/TemplateiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateiOSApp.swift
+//  TemplateiOS
+//
+//  Created by Wednesday on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TemplateiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
