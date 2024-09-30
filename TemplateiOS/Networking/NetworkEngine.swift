6c4770cbd8f8be2a0c1e0be38d9b32ef26e61bfe
sandesh-nk <sandesh.naik@allen.in>
2024-09-30 13:02:28 +0530

    Updated network request

---
+++ TemplateiOS/Networking/NetworkEngine.swift
@@ -0,0 +1,77 @@
+//
+//  NetworkEngine.swift
+//  Template
+//
+//  Created by Rameez Khan on 17/10/21.
+//
+
+import Foundation
+import Combine
+
+final class NetworkEngine {
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
+    
+    ///  Makes network request for given endpoint
+    ///  - Parameters:
+    ///  - endpoint: instance of type `Endpoint` representing url request endpoint
+    ///  - Returns: AnyPublisher<T, NetworkError>, where T is Decodable
+    func makeRequest<T: Decodable>(for endpoint: Endpoint) async -> AnyPublisher<T, NetworkError> {
+        
+        guard let urlRequest = endpoint.generateURLRequest() else {
+            return Fail<T, NetworkError>(error: .inValidEndPoint)
+                .eraseToAnyPublisher()
+        }
+        
+        return urlSession.dataTaskPublisher(for: urlRequest)
+            .tryMap { data, response in
+                guard let urlResponse = response as? HTTPURLResponse else {
+                    throw NetworkError.invalidResponse
+                }
+                if urlResponse.statusCode == 200 {
+                    do {
+                        let requestedModel = try JSONDecoder().decode(T.self, from: data)
+                        return requestedModel
+                    } catch {
+                        throw NetworkError.parsingError
+                    }
+                } else { throw NetworkError.invalidResponse}
+            }
+            .map{ $0 }
+            .mapError { $0 as! NetworkError }
+            .eraseToAnyPublisher()
+    }
+    
+    ///  Makes network request for given endpoint
+    ///  - Parameters:
+    ///  - endpoint: instance of type `Endpoint` representing url request endpoint
+    ///  - Returns: Result<T, NetworkError>, where T is Decodable
+    func makeRequest<T: Decodable>(for endpoint: Endpoint) async -> Result<T, NetworkError> {
+        guard let urlRequest = endpoint.generateURLRequest() else {
+            return .failure(.inValidEndPoint)
+        }
+        
+        do {
+            let (data, response) = try await urlSession.data(for: urlRequest)
+            guard let urlResponse = response as? HTTPURLResponse else {
+                return .failure(.invalidResponse)
+            }
+            if urlResponse.statusCode == 200 {
+                do {
+                    let requestedModel = try JSONDecoder().decode(T.self, from: data)
+                    return .success(requestedModel)
+                } catch {
+                    return .failure(.parsingError)
+                }
+            } else {
+                return .failure(.failed(statusCode: urlResponse.statusCode))
+            }
+        } catch {
+            return .failure(.unknownError)
+        }
+    }
+}
+
